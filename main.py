73a472ce8a1c696666423ed29e8dd3b598b17bc2
n1k0 <143019576+justn1k0@users.noreply.github.com>
2023-09-12 19:26:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import discord
+from discord.ext import commands
 
-bot = commands.Bot()
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', intents=intents)
+
 @bot.event
 async def on_ready():
-    print('Python Code Has Been Sterted To Log Messages As {0.user}'.format(bot))
+    print('Python Code Has Been Started To Log Messages As {0.user}'.format(bot))
     print("Bot Will Collect Every Single Message In Every Single Server's Channel If Bot Have Permission")
 
-@client.event
+@bot.event
 async def on_message(message):
-  print('==============================================')
-  print(message.author.name
+    try:
+        print('==============================================')
+        print(message.author.name + " said:")
+        print(message.content)
+        print('In ' + message.guild.name)
+        print('==============================================')
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+bot.run('TOKEN!')
